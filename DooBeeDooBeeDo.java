11f10b315eedce897e350cf5af7b922303d7e7e0
Meaghan <meaghan.rea@gmail.com>
2017-05-25 16:25:40 -0400

    DooBeeDooBeeDoo exercise

---
+++ DooBeeDooBeeDo.java
@@ -0,0 +1,19 @@
+
+public class DooBeeDooBeeDo {
+
+	public static void main(String[] args) {
+		int x = 1;
+		while (x<3){
+			System.out.print("Doo");
+			System.out.println("Bee");
+			System.out.println("Bop Pop Bla Blah");
+			x = x + 1;
+		}
+		if (x == 3) {
+			System.out.println("Do Do");
+			System.out.print("Hi Hoooooo Silver");
+		}
+
+	}
+
+}
