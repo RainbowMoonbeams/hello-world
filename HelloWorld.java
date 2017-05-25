b480edafa0f62dca69deea93d8287b27333146ca
Meaghan <meaghan.rea@gmail.com>
2017-05-25 16:16:49 -0400

    uploaded hello world

---
+++ HelloWorld.java
@@ -0,0 +1,41 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+    	String message = "hi hi hi";
+System.out.println(message);
+System.out.println("hello world");
+message = "I am Sam";
+System.out.println(message);
+message = "I am Spartacus";
+System.out.println(message);
+message = "My name is Meaghan";
+System.out.println(message);
+message = "This is easy once you get it";
+System.out.println(message);
+message = "hello hello hello";
+System.out.println(message);
+message = "123456";
+System.out.println(message);
+message = "hi, hello";
+message = "hello, hi";
+System.out.println(message );
+
+boolean writingCodeIsFun = true;
+boolean rootCanalsAreFun = false;
+
+System.out.println ("Is writing code fun?");
+System.out.println(writingCodeIsFun);
+
+System.out.println ("Root canals are fun");
+System.out.println(rootCanalsAreFun);
+
+System.out.println("What is the answer?");
+int myInt = 42;
+System.out.println(myInt);
+
+char sheSaidIShouldCallItM = 'a';
+System.out.println(sheSaidIShouldCallItM);
+	}
+
+}
