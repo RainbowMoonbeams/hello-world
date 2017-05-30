b8c4f243d8835a151c359bcb0e88f0b5314f1365
Meaghan <meaghan.rea@gmail.com>
2017-05-30 19:25:42 -0400

    Carnie

---
+++ Carnie.java
@@ -0,0 +1,17 @@
+
+public class Carnie {
+
+	public static void main(String[] args) {
+		int heightInInches = 67;
+
+		if (heightInInches >= 60) {
+			System.out.println("You're lucky!");
+			System.out.println("You're tall enough!");
+		} else if (heightInInches >= 30) {
+			System.out.println("You can ride the tea cups - they are so much fun!");
+		} else {
+			System.out.println("You're too short :(");
+		}
+
+	}
+}
