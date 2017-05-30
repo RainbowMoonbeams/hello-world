569392ab18a49b4b2419b6d5b969e4c083c89263
Meaghan <meaghan.rea@gmail.com>
2017-05-30 19:30:37 -0400

    Diapers

---
+++ Diapers.java
@@ -0,0 +1,18 @@
+
+public class Diapers {
+
+	public static void main(String[] args) {
+		// Pack of diapers cost $8.
+		int diapers = 8;
+
+		// I have $40.
+		int iHave = 40;
+
+		// How many packs of diapers can I purchase?
+		int iCanPurchase = 40 / 8;
+
+		System.out.println("I can purchase " + iCanPurchase + " packs of diapers.");
+
+	}
+
+}
