bf5ba16712bf0011df39e7f88539509d0082d300
Meaghan <meaghan.rea@gmail.com>
2017-05-30 19:27:55 -0400

    Color

---
+++ Color.java
@@ -0,0 +1,23 @@
+
+public class Color {
+
+	public static void main(String[] args) {
+		// if/else example with colors
+
+		String color = "purple";
+		{
+
+			if (color.equals("red")) {
+				System.out.println("I am red.");
+			} else if (color.equals("aquamarine")) {
+				System.out.println("It's a little blue and a little green");
+			} else if (color.equals("purple")) {
+				System.out.println("Purple is the best color!");
+			} else {
+				System.out.println("This is a great color too!");
+			}
+
+		}
+
+	}
+}
