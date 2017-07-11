d7c2b4fe832857d251f3ade0459d984b60d0ce15
Meaghan <meaghan.rea@gmail.com>
2017-07-11 16:49:13 -0400

    edits to Greater of Two

--- src/main/java/GreaterOfTwo.java
+++ src/main/java/GreaterOfTwo.java
@@ -3,24 +3,23 @@ import java.util.Scanner;
 public class GreaterOfTwo {
 
 	public static void main(String[] args) {
-		//Ask the user to input two integer values. Print the greater value.
-		//Extension: ask the user for three integers, then print the greatest value.
-		
-		Scanner input = new Scanner (System.in);
-		
+		// Ask the user to input two integer values. Print the greater value.
+		// Extension: ask the user for three integers, then print the greatest value.
+
+		Scanner input = new Scanner(System.in);
+
 		System.out.println("Give me a number.");
-		int number1 = input.nextInt();
-		
+		int firstNumber = input.nextInt();
+
 		System.out.println("Give me another number.");
-		int number2 = input.nextInt();
-		
-		int greaterNumber = (number1 > number2); {
-		System.out.println(+ " is the greater number.");
-		}
-		if (number1 > number2); {
-		System.out.println(number1 + " is the greater number.");
+		int secondNumber = input.nextInt();
+
+		boolean greaterNumber = (firstNumber > secondNumber);
+
+		if (firstNumber > secondNumber)	{
+			System.out.println(firstNumber + " is the greater number.");
 		} else {
-		System.out.println(number2 + " is the greater number.");
-}
+			System.out.println(secondNumber + " is the greater number.");
+		}
 	}
 }
